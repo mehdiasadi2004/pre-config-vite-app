cad7f363e92597c52711aaa06ed9aa002d9693e3
mehdi asadi <mmasadi2004@gmail.com>
2025-04-24 12:39:07 +0330

    set provoders

--- src/provider.tsx
+++ src/provider.tsx
@@ -2,19 +2,32 @@ import type { NavigateOptions } from "react-router-dom";
 
 import { HeroUIProvider } from "@heroui/system";
 import { useHref, useNavigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 declare module "@react-types/shared" {
   interface RouterConfig {
     routerOptions: NavigateOptions;
   }
 }
-
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
 export function Provider({ children }: { children: React.ReactNode }) {
   const navigate = useNavigate();
 
   return (
     <HeroUIProvider navigate={navigate} useHref={useHref}>
-      {children}
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <Toaster />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </HeroUIProvider>
   );
 }
